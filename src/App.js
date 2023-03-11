650b5239585ec4779f117774dc71191092ca4a26
janaugstein <jan_augstein@yahoo.de>
2023-03-11 16:55:54 +0100

    first setUp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function createSession() {
+  alert("createSession");
+}
+
+function joinSession() {
+  alert("joinSession");
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={createSession}>Create Session</button>
+      <button onClick={joinSession}>Join Session</button>
     </div>
   );
 }
