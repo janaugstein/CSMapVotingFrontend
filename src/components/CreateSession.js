e718aeecd3fe8a72af750783a1e83c907f6d7f3b
janaugstein <jan_augstein@yahoo.de>
2023-03-14 11:44:16 +0100

    finished the createSession page

--- src/components/CreateSession.js
+++ src/components/CreateSession.js
@@ -2,15 +2,44 @@ import TextField from "@mui/material/TextField";
 import { useRef } from "react";
 import axios from "axios";
 import "./CreateSession.css";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 function CreateSession() {
   const nameRef = useRef();
   const map1Ref = useRef();
   const map2Ref = useRef();
   const map3Ref = useRef();
+  const [cookies, setCookie] = useCookies(["joined"]);
+  const navigate = useNavigate();
 
   function create() {
-    alert("clicked create");
+    let maps = [
+      map1Ref.current.value,
+      map2Ref.current.value,
+      map3Ref.current.value,
+    ];
+    console.log(maps);
+    axios
+      .post("http://localhost:8001/createSession", {
+        maps: maps,
+        name: nameRef.current.value,
+      })
+      .then(function (response) {
+        let d = new Date();
+        //expiration after 30 minutes
+        d.setTime(d.getTime() + 30 * 60 * 1000);
+        console.log(response.data.sessionID);
+        setCookie(
+          "joined",
+          { name: nameRef.current.value, sessionID: response.data.sessionID },
+          { path: "/", expires: d, sameSite: "none" }
+        );
+        navigate("/voting");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   return (
